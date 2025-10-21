37c01c3627398adc3cf72eb82c7f3c2fc57569dc
Daniel Dinev <azsumbg@gmail.com>
2025-10-21 18:05:15 +0300

    added UNITS class methods and RANDIT class

--- PLATFORM/platform.cpp
+++ PLATFORM/platform.cpp
@@ -1,6 +1,36 @@
 #include "pch.h"
 #include "platform.h"
 
+// RANDIT ************************
+
+dll::RANDIT::RANDIT()
+{
+	std::random_device rd{};
+	std::seed_seq sq{ rd(),rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+	twister = new std::mt19937(sq);
+}
+dll::RANDIT::RANDIT(RANDIT& other)
+{
+	std::random_device rd{};
+	std::seed_seq sq{ rd(),rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+	twister = new std::mt19937(sq);
+}
+dll::RANDIT::~RANDIT()
+{
+	if (twister)delete twister;
+}
+
+int dll::RANDIT::operator()(int min, int max)
+{
+	std::uniform_int_distribution distrib(min, max);
+
+	return distrib((*twister));
+}
+
+//////////////////////////////////
+
 // PROTON ***********************
 
 dll::PROTON::PROTON()
@@ -106,19 +136,322 @@ float dll::PROTON::GetHeight() const
 
 /////////////////////////////////
 
+// ASSETS **********************
+
+dll::ASSETS::ASSETS(obstacle what_type, float start_x, float start_y) :PROTON(start_x, start_y)
+{
+	type = what_type;
+
+	switch (type)
+	{
+	case obstacle::rock:
+		NewDims(100.0f, 100.0f);
+		break;
+
+	case obstacle::orc_castle:
+		NewDims(210.0f, 200.0f);
+		goods_delay = 100;
+		goods_in_asset = 1000;
+		lifes = 1000;
+		break;
+
+	case obstacle::human_castle:
+		NewDims(260.0f, 260.0f);
+		goods_delay = 100;
+		goods_in_asset = 1000;
+		lifes = 1200;
+		max_frames = 7;
+		frame_delay = 9;
+		break;
+
+	case obstacle::small_tree:
+		NewDims(47.0f, 80.0f);
+		goods_delay = 60;
+		lifes = 60;
+		goods_in_asset = 10;
+		break;
+
+	case obstacle::mid_tree:
+		NewDims(78.0f, 86.0f);
+		goods_delay = 80;
+		lifes = 80;
+		goods_in_asset = 15;
+		break;
+
+	case obstacle::big_tree:
+		NewDims(98.0f, 90.0f);
+		goods_delay = 90;
+		lifes = 100;
+		goods_in_asset = 20;
+		break;
+
+	case obstacle::mine:
+		NewDims(261.0f, 363.0f);
+		goods_delay = 100;
+		lifes = 3000;
+		goods_in_asset = 50;
+		max_frames = 124;
+		frame_delay = 1;
+		break;
+	}
+
+	max_frame_delay = frame_delay;
+	max_goods_delay = goods_delay;
+}
+int dll::ASSETS::GetFrame()
+{
+	frame_delay--;
+	if (frame_delay <= 0)
+	{
+		frame_delay = max_frame_delay;
+		++frame;
+		if (frame > max_frames)frame = 0;
+	}
+	return frame;
+}
+int dll::ASSETS::GetGoods()
+{
+	if (goods_delay > 0)goods_delay--;
+	else
+	{
+		goods_delay = max_goods_delay;
+		lifes--;
+		if (lifes <= 0)return goods_in_asset;
+	}
+
+	return 0;
+}
+void dll::ASSETS::Release()
+{
+	delete this;
+}
+
+////////////////////////////////
 
 
+// UNITS **********************
 
+dll::UNITS::UNITS(unit_type what, float start_x, float start_y)
+{
+	type = what;
 
+	RANDIT rand_gen{};
 
+	switch (type)
+	{
+	case unit_type::peon:
+		lifes = 50 + rand_gen(0, 20);
+		speed = 0.5f + (float)(rand_gen(0, 2)) / 10.0f;
+		strenght = 5 + rand_gen(0, 5);
+		range = 150.0f;
+		max_frames = 40;
+		max_frame_delay = 2;
+		frame_delay = max_frame_delay;
+		attack_delay = 100;
+		break;
 
+	case unit_type::orc_warrior:
+		lifes = 100 + rand_gen(0, 30);
+		speed = 0.8f + (float)(rand_gen(0, 3)) / 10.0f;
+		strenght = 25 + rand_gen(0, 5);
+		range = 200.0f;
+		max_frames = 9;
+		max_frame_delay = 7;
+		frame_delay = max_frame_delay;
+		attack_delay = 80;
+		break;
 
+	case unit_type::orc_knight:
+		lifes = 100 + rand_gen(0, 30);
+		speed = 0.4f + (float)(rand_gen(0, 3)) / 10.0f;
+		strenght = 50 + rand_gen(0, 10);
+		range = 200.0f;
+		max_frames = 30;
+		max_frame_delay = 2;
+		frame_delay = max_frame_delay;
+		attack_delay = 80;
+		break;
 
+	case unit_type::orc_archer:
+		lifes = 70 + rand_gen(0, 30);
+		speed = 0.6f + (float)(rand_gen(0, 3)) / 10.0f;
+		strenght = 40 + rand_gen(0, 5);
+		range = 250.0f;
+		max_frames = 18;
+		max_frame_delay = 3;
+		frame_delay = max_frame_delay;
+		attack_delay = 120;
+		break;
 
+	case unit_type::peasant:
+		lifes = 50 + rand_gen(0, 20);
+		speed = 0.5f + (float)(rand_gen(0, 2)) / 10.0f;
+		strenght = 5 + rand_gen(0, 5);
+		range = 150.0f;
+		max_frames = 19;
+		max_frame_delay = 4;
+		frame_delay = max_frame_delay;
+		attack_delay = 100;
+		break;
 
+	case unit_type::warrior:
+		lifes = 100 + rand_gen(0, 30);
+		speed = 0.8f + (float)(rand_gen(0, 3)) / 10.0f;
+		strenght = 25 + rand_gen(0, 5);
+		range = 200.0f;
+		max_frames = 3;
+		max_frame_delay = 10;
+		frame_delay = max_frame_delay;
+		attack_delay = 80;
+		break;
 
+	case unit_type::knight:
+		lifes = 100 + rand_gen(0, 30);
+		speed = 0.4f + (float)(rand_gen(0, 3)) / 10.0f;
+		strenght = 50 + rand_gen(0, 10);
+		range = 200.0f;
+		max_frames = 24;
+		max_frame_delay = 3;
+		frame_delay = max_frame_delay;
+		attack_delay = 80;
+		break;
 
+	case unit_type::archer:
+		lifes = 70 + rand_gen(0, 30);
+		speed = 0.6f + (float)(rand_gen(0, 3)) / 10.0f;
+		strenght = 40 + rand_gen(0, 5);
+		range = 250.0f;
+		max_frames = 41;
+		max_frame_delay = 2;
+		frame_delay = max_frame_delay;
+		attack_delay = 120;
+		break;
+	}
 
+	max_attack_delay = attack_delay;
+}
+void dll::UNITS::SetPath(float _where_x, float _where_y)
+{
+	hor_path = false;
+	vert_path = false;
+
+	start_move_x = start.x;
+	start_move_y = start.y;
+
+	final_move_x = _where_x;
+	final_move_y = _where_y;
+
+	if (start_move_x < final_move_x)dir = dirs::right;
+	else dir = dirs::left;
+
+	if (final_move_x == start_move_x || (final_move_x > start_move_x && final_move_x < end.x))
+	{
+		vert_path = true;
+		return;
+	}
+	if (final_move_y == start_move_y || (final_move_y > start_move_y && final_move_y < end.y))
+	{
+		hor_path = true;
+		return;
+	}
+
+	slope = (final_move_y - start_move_y) / (final_move_x - start_move_x);
+	intercept = start_move_y - start_move_x * slope;
+}
+void dll::UNITS::Release()
+{
+	delete this;
+}
+int dll::UNITS::GetFrame()
+{
+	frame_delay--;
+	if (frame_delay <= 0)
+	{
+		frame_delay = max_frame_delay;
+		++frame;
+		if (frame > max_frames)frame = 0;
+	}
+	return frame;
+}
+void dll::UNITS::Move(float gear, BAG<ASSETS>obstacles)
+{
+	float now_speed = speed * gear / 10.0f;
+
+	if (vert_path)
+	{
+		if (start_move_y > final_move_y)
+		{
+			if (start.y - now_speed >= sky)
+			{
+				start.y -= now_speed;
+				SetEdges();
+				return;
+			}
+		}
+		else if (end.y + now_speed <= ground)
+		{
+			start.y += now_speed;
+			SetEdges();
+			return;
+		}
+	}
+	if (hor_path)
+	{
+		if (start_move_x > final_move_x)
+		{
+			if (start.x - now_speed >= 0)
+			{
+				start.x -= now_speed;
+				SetEdges();
+				return;
+			}
+		}
+		else if (end.x + now_speed <= scr_width)
+		{
+			start.x += now_speed;
+			SetEdges();
+			return;
+		}
+	}
+
+	if (start_move_x > final_move_x)
+	{
+		float next_y = (start.x - now_speed) * slope + intercept;
+
+		if (start.x - now_speed >= 0 && next_y >= sky && next_y + GetHeight() <= ground)
+		{
+			start.x -= now_speed;
+			start.y = start.x * slope + intercept;
+			SetEdges();
+			return;
+		}
+	}
+	if (start_move_x < final_move_x)
+	{
+		float next_y = (start.x + now_speed) * slope + intercept;
+
+		if (start.x + now_speed >= 0 && next_y >= sky && next_y + GetHeight() <= ground)
+		{
+			start.x += now_speed;
+			start.y = start.x * slope + intercept;
+			SetEdges();
+			return;
+		}
+	}
+	
+}
+int dll::UNITS::Attack()
+{
+	--attack_delay;
+	if (attack_delay <= 0)
+	{
+		attack_delay = max_attack_delay;
+		return strenght;
+	}
+	return 0;
+}
+
+////////////////////////////////
 
 // FUNCTIONS *******************************************
 
@@ -128,4 +461,51 @@ bool dll::Intersect(FRECT first, FRECT second)
 		|| first.up_left.y >= second.down_left.y || first.down_left.y <= second.up_left.y))return true;
 
 	return false;
+}
+float dll::Distance(FPOINT first, FPOINT second)
+{
+	float a = (float)(pow(abs(second.x - first.x), 2));
+	float b = (float)(pow(abs(second.y - first.y), 2));
+
+	return (float)(sqrt(a + b));
+}
+void dll::Sort(BAG<UNITS>& army, FPOINT target)
+{
+	if (army.size() < 2)return;
+
+	bool is_ok = false;
+
+	while (!is_ok)
+	{
+		is_ok = true;
+
+		for (size_t count = 0; count < army.size() - 1; ++count)
+		{
+			if (Distance(army[count].center, target) > Distance(army[count + 1].center, target))
+			{
+				UNITS temp = army[count];
+				army[count] = army[count + 1];
+				army[count + 1] = temp;
+				is_ok = false;
+			}
+		}
+	}
+}
+
+
+PLATFORM_API dll::ASSETS* dll::AssetFactory(obstacle what, float startx, float starty)
+{
+	ASSETS* ret{ nullptr };
+	ret = new ASSETS(what, startx, starty);
+
+	return ret;
+}
+
+PLATFORM_API dll::UNITS* dll::UnitFactory(unit_type what_unit, float startx, float starty)
+{
+	UNITS* ret{ nullptr };
+
+	ret = new UNITS(what_unit, startx, starty);
+
+	return ret;
 }
--- PLATFORM/platform.h
+++ PLATFORM/platform.h
@@ -17,8 +17,11 @@ constexpr float ground { 750.0f };
 
 enum class dirs { left = 0, right = 1, up = 2, down = 3, stop = 4 };
 enum class actions { stop = 0, move = 1, flee = 2, harvest = 3, shoot = 4, melee = 5 };
-enum class unit_type { peon = 0, warrior = 1, archer = 2, knight = 3, artillery = 4 };
-enum class obstacle { rock = 0, small_tree = 1, mid_tree = 2, big_tree = 3 };
+enum class unit_type {
+	peon = 0, orc_warrior = 1, orc_archer = 2, orc_knight = 3, artillery = 4,
+	peasant = 5, warrior = 6, archer = 7, knight = 8
+};
+enum class obstacle { rock = 0, small_tree = 1, mid_tree = 2, big_tree = 3, orc_castle = 4, human_castle = 5, mine = 6 };
 
 struct FPOINT
 {
@@ -144,16 +147,53 @@ namespace dll
 			}
 		}
 
-		BAG& operator [] (size_t index)
+		T& operator [] (size_t index)
 		{
 			if (index >= next_pos) throw std::runtime_error("Wrong index !");
 			if (!m_ptr) throw std::runtime_error("Invalid data pointer !");
 
 			return m_ptr[index];
 		}
+
+		BAG& operator =(BAG& other)
+		{
+			if (m_ptr)
+			{
+				if (this->m_ptr == other.m_ptr)return other;
+
+				if (this->max_size >= other.max_size)
+				{
+					for (size_t count = 0; count < other.next_pos; ++count)m_ptr[count] = other[count];
+				}
+				else
+				{
+					max_size = other.max_size;
+
+					m_ptr = reinterpret_cast<T*>(realloc(m_ptr, max_size * sizeof(T)));
+					if (m_ptr)
+					{
+						for (size_t count = 0; count < other.next_pos; ++count)m_ptr[count] = other[count];
+					}
+				}
+			}
+		}
 	};
 	
+	/// ////////////////////////////////////////////
 	
+	class PLATFORM_API RANDIT
+	{
+	private:
+		std::mt19937* twister{ nullptr };
+
+	public:
+		RANDIT();
+		RANDIT(RANDIT& other);
+		~RANDIT();
+
+		int operator()(int min, int max);
+	};
+
 	class PLATFORM_API PROTON
 	{
 	protected:
@@ -183,19 +223,137 @@ namespace dll
 		float GetHeight() const;
 	};
 
+	class PLATFORM_API ASSETS :public PROTON
+	{
+	private:
+		int goods_in_asset{ 0 };
+		int goods_delay = 0;
+		int max_goods_delay;
 
+		int max_frames{ 0 };
+		int frame{ 0 };
+		int frame_delay{ 0 };
+		int max_frame_delay{ 0 };
 
+		ASSETS(obstacle what_type, float start_x, float start_y);
 
+	public:
+		obstacle type;
+		int lifes = 0;
 
+		int GetFrame();
+		int GetGoods();
+		void Release();
 
+		friend PLATFORM_API ASSETS* AssetFactory(obstacle what, float startx, float starty);
+	};
 
+	class PLATFORM_API UNITS :public PROTON
+	{
+	private:
+		float speed{ 0 };
+		float strenght{ 0 };
+		float range{ 0 };
 
+		int max_frames{ 0 };
+		int frame{ 0 };
+		int frame_delay{ 0 };
+		int max_frame_delay{ 0 };
+
+		int attack_delay{ 0 };
+		int max_attack_delay{ 0 };
+
+		bool hor_path{ false };
+		bool vert_path{ false };
+		float slope{ 0 };
+		float intercept{ 0 };
+		
+		float start_move_x{ 0 };
+		float start_move_y{ 0 };
+		float final_move_x{ 0 };
+		float final_move_y{ 0 };
+
+		UNITS(unit_type what, float start_x, float start_y);
+
+		void SetPath(float _where_x, float _where_y);
+
+	public:
+		unit_type type{ unit_type::peon };
+		dirs dir{ dirs::stop };
+		
+		int lifes{ 0 };
+
+		virtual ~UNITS() {};
+		void Release();
+
+		int GetFrame();
+
+		void Move(float gear, BAG<ASSETS>obstacles);
+		int Attack();
+
+		friend PLATFORM_API actions AINextMove(UNITS my_unit, BAG<UNITS>& BadArmy, BAG<ASSETS>Obstacles);
+		friend PLATFORM_API UNITS* UnitFactory(unit_type what_unit, float startx, float starty);	
+	};
 
 	// FUNCTIONS *******************************************
 
 	template<typename T>concept primes = std::is_same<T, int>::value || std::is_same<T, float>::value
 		|| std::is_same<T, double>::value || std::is_same<T, long>::value || std::is_same<T, char>::value;
 
-	bool PLATFORM_API Intersect(FRECT first, FRECT second);
+	template<primes T> void Sort(BAG<T>& bag, bool ascending = true)
+	{
+		if (bag.size() > 1)
+		{
+			if (ascending)
+			{
+				bool is_ok = false;
 
+				while (!is_ok)
+				{
+					is_ok = true;
+
+					for (size_t count = 0; count < bag.size() - 1; ++count)
+					{
+						if (bag[count] > bag[count + 1])
+						{
+							T temp = bag[count];
+							bag[count] = bag[count + 1];
+							bag[count + 1] = temp;
+							is_ok = false;
+						}
+					}
+				}
+			}
+			else
+			{
+				bool is_ok = false;
+
+				while (!is_ok)
+				{
+					is_ok = true;
+
+					for (size_t count = 0; count < bag.size() - 1; ++count)
+					{
+						if (bag[count] < bag[count + 1])
+						{
+							T temp = bag[count];
+							bag[count] = bag[count + 1];
+							bag[count + 1] = temp;
+							is_ok = false;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	bool PLATFORM_API Intersect(FRECT first, FRECT second);
+	float PLATFORM_API Distance(FPOINT first, FPOINT second);
+	void PLATFORM_API Sort(BAG<UNITS>& army, FPOINT target);
+
+	PLATFORM_API actions AINextMove(UNITS my_unit, BAG<UNITS>& BadArmy, BAG<ASSETS>Obstacles);
+
+	PLATFORM_API ASSETS* AssetFactory(obstacle what, float startx, float starty);
+
+	PLATFORM_API UNITS* UnitFactory(unit_type what_unit, float startx, float starty);
 }
